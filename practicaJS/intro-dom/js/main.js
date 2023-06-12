97abe5bcb040a54c6af76e82be97403c756580b5
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-06-11 22:02:19 -0300

    main
    ejercicio 2

--- practicaJS/intro-dom/js/main.js
+++ practicaJS/intro-dom/js/main.js
@@ -3,4 +3,7 @@ window.onload = () =>{
     const titulo = document.getElementById('titulo');
     console.log(titulo);
     titulo.innerText = 'Texto actualizado desde main.js';
+
+    const parrafo = document.getElementById('parrafo');
+    parrafo.innerHTML = '<li>elemento 1</li><li>elemento 2</li>'
 }
